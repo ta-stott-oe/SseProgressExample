c474379078a7b321ddd48e802fde9bfa9a0367c9
Tim Stott <tstott@oxfordeconomics.com>
2016-10-18 20:48:25 +0100

    Added JobController

---
+++ SseProgressExample/Controllers/JobController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SseProgressExample.Controllers
+{
+    [RoutePrefix("api/job")]
+    public class JobController : ApiController
+    {
+        [Route("")]
+        public HttpResponseMessage Put()
+        {
+            var guid = Guid.NewGuid();
+            return Request.CreateResponse(guid.ToString());
+        }
+
+        [Route("{id}")]
+        public HttpResponseMessage Get(string id)
+        {
+            return Request.CreateResponse("get");
+        }
+    }
+}
